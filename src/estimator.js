57dc53629cdfe72f9edead87a31b724bab928185
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-04-10 17:55:09 +0200

    estimate cases and beds

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,13 @@
-const infectionsRate = (data, impactFactor) => {
+const infectionsRate = (data, impactFactors) => {
   const duration = {
     days: 1,
     weeks: 7,
     months: 30
   };
-  return data.reportedCases
-  * impactFactor
-  * 2 ** parseInt((data.timeToElapse * duration[data.periodType]) / 3, 10);
+  return [data.reportedCases * 10, data.reportedCases
+  * impactFactors
+  * 2 ** parseInt((data.timeToElapse * duration[data.periodType]) / 3, 10),
+  parseInt(infectionsRate(data, impactFactors) * 0.15, 10)];
 };
 
 const covid19ImpactEstimator = (data) => {
@@ -14,12 +15,18 @@ const covid19ImpactEstimator = (data) => {
   return {
     data: input,
     impact: {
-      currentlyInfected: input.reportedCases * 10,
-      infectionsByRequestedTime: infectionsRate(input, 10)
+      currentlyInfected: infectionsRate(input, 10)[0],
+      infectionsByRequestedTime: infectionsRate(input, 10)[1],
+      severeCasesByRequestedTime: infectionsRate(input, 10)[2],
+      hospitalBedsByRequestedTime: parseInt(input.totalHospitalBeds * 0.35, 10)
+      - infectionsRate(input, 10)[2]
     },
     severeImpact: {
-      currentlyInfected: input.reportedCases * 50,
-      infectionsByRequestedTime: infectionsRate(input, 50)
+      currentlyInfected: infectionsRate(input, 50)[0],
+      infectionsByRequestedTime: infectionsRate(input, 50)[1],
+      severeCasesByRequestedTime: infectionsRate(input, 50)[2],
+      hospitalBedsByRequestedTime: parseInt(input.totalHospitalBeds * 0.35, 10)
+      - infectionsRate(input, 50)[2]
     }
   };
 };
