f3123d19a8339641f48e011fa4510babd90cc902
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-04-10 16:42:14 +0200

    use func to calculate

--- src/estimator.js
+++ src/estimator.js
@@ -1,27 +1,25 @@
-const covid19ImpactEstimator = (data) => {
-  const input = data;
-
+const infectionsRate = (data, impactFactor) => {
   const duration = {
     days: 1,
     weeks: 7,
     months: 30
   };
+  return data.reportedCases
+  * impactFactor
+  * 2 ** parseInt((data.timeToElapse * duration[data.periodType]) / 3, 10)
+};
 
+const covid19ImpactEstimator = (data) => {
+  const input = data;
   return {
     data: input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
-      infectionsByRequestedTime:
-        input.reportedCases
-        * 10
-        * 2 ** parseInt((input.timeToElapse * duration[input.periodType]) / 3, 10)
+      infectionsByRequestedTime: infectionsRate(input, 10)
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
-      infectionsByRequestedTime:
-        input.reportedCases
-        * 50
-        * 2 ** parseInt((input.timeToElapse * duration[input.periodType]) / 3, 10)
+      infectionsByRequestedTime: infectionsRate(input, 50)
     }
   };
 };
