f990fa4d61db3530727d5e95617e7f4c87ca835f
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-04-10 03:58:22 +0200

    calculate infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -3,12 +3,18 @@ const covid19ImpactEstimator = (data) => {
   return {
     data: input,
     impact: {
-      currentlyInfected: input.reportedCases * 10
+      currentlyInfected: input.reportedCases * 10,
+      infectionsByRequestedTime: infectionsRate(input)
     },
     severeImpact: {
-      currentlyInfected: input.reportedCases * 50
+      currentlyInfected: input.reportedCases * 50,
+      infectionsByRequestedTime: infectionsRate(input)
     }
   };
+};
+
+const infectionsRate = (data) => {
+  return 5;
 };
 
 export default covid19ImpactEstimator;
